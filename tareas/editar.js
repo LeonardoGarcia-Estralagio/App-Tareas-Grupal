7ccb1c2eea90926503c8fe216acb1ce6431183b0
Debora <escobardebo@gmail.com>
2023-06-01 22:14:36 -0300

    Agrega funcion editar

---
+++ tareas/editar.js
@@ -0,0 +1,19 @@
+const { leerJSON } = require("../data");
+
+
+const editar = function (clase, estado) {
+    const tareas = leerJSON()
+    const tarea = tareas.find((tarea) => tarea.clase === clase);
+    if (!tarea) {
+      return `ERROR:La clase ${clase} no se encuentra`;
+    }
+    const tareasActualizadas = tareas.map((tarea) => {
+      if (tarea.clase === clase) {
+        tarea.estado = estado.toLowerCase();
+      }
+      return tarea;
+    });
+    escribirJSON(tareasActualizadas);
+    return `El estado de la clase ${tarea.titulo} ahora es ${estado}`;
+  }
+  module.exports = editar
